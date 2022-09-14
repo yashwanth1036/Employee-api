80448b58df79b36318a1b4e85ba93e0075c78e61
yashwanth1036 <74044207+yashwanth1036@users.noreply.github.com>
2022-09-14 21:48:22 +0530

    added adition in user controller

--- src/main/java/com/yjp/employeeapi/controller/UserController.java
+++ src/main/java/com/yjp/employeeapi/controller/UserController.java
@@ -19,8 +19,10 @@ public class UserController {
     @GetMapping("/allUsers")
     public ResponseEntity<List<Users>> getALlUsers(){
         List<Users> users = null;
+        int a=10,b=20,sum;
         try{
             users = userService.getALlUsers();
+            sum = a + b;
         }catch (Exception ge){
             ge.getMessage();
         }
